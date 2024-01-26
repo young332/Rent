738feabb74f5cc903539ec7cc9ac249df4cc7802
spaisirl <149464817+spaisirl@users.noreply.github.com>
2024-01-26 14:11:12 +0900

    PaymentVO 에 initSaleTotal 메서드 추가

--- src/main/java/com/kh/rent/checkout/domain/PaymentVO.java
+++ src/main/java/com/kh/rent/checkout/domain/PaymentVO.java
@@ -22,5 +22,18 @@ public class PaymentVO {
 		private int salePrice;
 		private int totalPrice;
 		
+		@Override
+		public String toString() {
+			return "PaymentVO [pay_type=" + pay_type + ", pay_res_rid=" + pay_res_rid + ", pay_mem_id=" + pay_mem_id
+					+ ", pay_date=" + pay_date + ", pay_cost=" + pay_cost + ", salePrice=" + salePrice + ", totalPrice="
+					+ totalPrice + ", toString()=" + super.toString() + "]";
+		}
+		
+		public void initSaleTotal() {
+			this.salePrice = (int) (this.pay_cost * 1);
+			this.totalPrice = this.salePrice;
+			
+		}
+		
 
 }
