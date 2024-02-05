8b108e0f2b597abf9402237cc75205a0561e0948
HW22222 <ppop7377@naver.com>
2024-02-05 18:19:53 +0900

    hyewon - myPage Controller commit

--- src/main/java/com/kh/rent/myPage/controller/MyPageController.java
+++ src/main/java/com/kh/rent/myPage/controller/MyPageController.java
@@ -20,6 +20,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.kh.rent.login.domain.MemberVO;
+import com.kh.rent.myPage.domain.GetCarNameDTO;
 import com.kh.rent.myPage.domain.PWchangeDTO;
 import com.kh.rent.myPage.service.MyPageService;
 import com.kh.rent.reserve.domain.ReserveVO;
@@ -40,9 +41,23 @@ public class MyPageController {
 		MemberVO loginInfo = (MemberVO)session.getAttribute("loginInfo");
 		String mem_id = loginInfo.getMem_id();
 		List<ReserveVO> reserveList = myPageService.getReserveList(mem_id);
+		
+		String[] carNames = new String[reserveList.size()];
+		for (int i = 0; i < reserveList.size(); i++) {
+			int rid = reserveList.get(i).getRes_rid();
+			String car_id = reserveList.get(i).getRes_car_id();
+			GetCarNameDTO getCarNameDTO = GetCarNameDTO.builder()
+					.res_rid(rid)
+					.res_car_id(car_id)
+					.build();
+			String car_name = myPageService.getCarName(getCarNameDTO);
+			carNames[i] = car_name;
+		} 
 		model.addAttribute("reserveList", reserveList);
+		model.addAttribute("carNames", carNames);
 		log.info("reservationListGet..");
 		log.info("reserveList" + reserveList);
+		log.info("carNames" + carNames);
 	}
 	
 	// 마이페이지
