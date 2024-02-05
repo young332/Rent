ab9899cc7f8e8c703cc01e72b53791f580ba7d1a
samsung <samsung@BOOK-3GJRA9JM99>
2024-02-05 21:53:07 +0900

    하연비회원작업쪼금

--- src/main/java/com/kh/rent/reserve/mapper/NonMemberMapper.java
+++ src/main/java/com/kh/rent/reserve/mapper/NonMemberMapper.java
@@ -1,7 +1,10 @@
 package com.kh.rent.reserve.mapper;
 
+import java.util.List;
+
 import com.kh.rent.reserve.domain.NonMemberVO;
 
 public interface NonMemberMapper {
-	public void nonresinsert(NonMemberVO nonmemberVO);
+	public void nonmeminsert(NonMemberVO nonmemberVO);
+	public List<NonMemberVO> getNonlist();
 }
