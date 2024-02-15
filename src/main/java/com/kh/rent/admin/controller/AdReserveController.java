998f4f7f8b6ffffe5f978626c584322a45710b96
soyoung <kerry0325@naver.com>
2024-02-15 11:53:04 +0900

    관리자페이지 예약내역확인 추가/회원관리-포인트,기능 추가

---
+++ src/main/java/com/kh/rent/admin/controller/AdReserveController.java
@@ -0,0 +1,14 @@
+package com.kh.rent.admin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.log4j.Log4j;
+
+@Controller
+@Log4j
+@RequestMapping("/admin/reserve/*")
+public class AdReserveController {
+	
+
+}
